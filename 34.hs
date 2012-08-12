05d3c69c1b685ef6ad826e20de943f7dc47dd2fb
Cody Veal <cveal05@gmail.com>
2012-08-12 02:47:28 -0400

    added totient function, question 34

---
+++ 34.hs
@@ -0,0 +1,9 @@
+myGCD :: Int -> Int -> Int
+myGCD n 0 = n
+myGCD n m = gcd m $ n `mod` m
+
+coprime :: Int -> Int -> Bool
+coprime n m = 1 == myGCD n m
+
+totient :: Int -> Int
+totient n = foldl (flip ((+) . fromEnum)) 0 (map (coprime n) [1..n])
